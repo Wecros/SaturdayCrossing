75c1a555d7edca4e76f7f34ebb26c6097cc1dd56
Wecros <wecros@gmail.com>
2020-08-29 13:15:35 +0200

    Add player movement and player objects

---
+++ godot-project/scripts/player-movement.gd
@@ -0,0 +1,32 @@
+extends KinematicBody2D
+
+export (int) var speed = 200
+
+var velocity = Vector2()
+
+var PLAYER_RIGHT
+var PLAYER_LEFT
+var PLAYER_UP
+var PLAYER_DOWN
+
+func _init():
+	PLAYER_RIGHT = 'player-1-right'
+	PLAYER_LEFT = 'player-1-left'
+	PLAYER_UP = 'player-1-up'
+	PLAYER_DOWN = 'player-1-down'
+
+func get_input():
+    velocity = Vector2()
+    if Input.is_action_pressed(PLAYER_RIGHT):
+        velocity.x += 1
+    if Input.is_action_pressed(PLAYER_LEFT):
+        velocity.x -= 1
+    if Input.is_action_pressed(PLAYER_DOWN):
+        velocity.y += 1
+    if Input.is_action_pressed(PLAYER_UP):
+        velocity.y -= 1
+    velocity = velocity.normalized() * speed
+
+func _physics_process(delta):
+    get_input()
+    velocity = move_and_slide(velocity)
---
+++ godot-project/scripts/player_2_movement.gd
@@ -0,0 +1,7 @@
+extends "res://scripts/player-movement.gd"
+
+func _init():
+	PLAYER_RIGHT = 'player-2-right'
+	PLAYER_LEFT = 'player-2-left'
+	PLAYER_UP = 'player-2-up'
+	PLAYER_DOWN = 'player-2-down'
